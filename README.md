4b059e59142ae7a7bf533890eb3849cc91c9de0a
mahesh0790 <mahesh.rockz790@gmail.com>
2018-05-07 04:19:35 +0000

    script to install jenkins on ec2-ubuntu-instance

---
+++ README.md
@@ -0,0 +1,47 @@
+# jenkins-ec2-https
+How to setup [Jenkins CI](https://jenkins.io/) on EC2 (Ubuntu 16.04 LTS) with https access.
+
+This task took me three hours reading a lot of web pages and lots of trial and error. To make your life easier this is what you need to do to setup a Jenkins CI instance using your AWS account.
+
+I've tested this process and it took exactly *5 minutes* to get to the https login screen of the new jenkins instance. Most of that time is waiting for the scripted installation of components on the AWS instance.
+
+## Create an EC2 instance
+* open AWS Console in browser
+* go to EC2
+* select **Launch Instance**
+* select **Ubuntu Server 16.04 LTS**
+* select **t2.micro** (I use **t2.small** to get more memory for my greedy build)
+* select **Configure Instance Details**
+* tick **Enable termination protection**
+* select **Add Storage**
+* change size to preferred (I use 100GB for our large enterprise builds)
+* select **Add Tags**
+* set value to *Jenkins*
+* select **Configure Security Group**
+* add rule *HTTPS*
+* select **Review and launch**
+* select a key pair
+* select **Launch Instance**
+* In EC2 go to instances, once instance running then select instance and click **Connect**
+* copy ssh command in the example to terminal and run (you'll need your referenced key file present in that directory)
+
+## Deploy jenkins
+Login to the instance using the ssh command mentioned above. Then run:
+```bash
+wget --no-cache https://raw.githubusercontent.com/davidmoten/jenkins-ec2-https/master/setup.sh && chmod +x setup.sh
+```
+Now edit the file `setup.sh` and edit the top block of parameters with the values you want to see in the generated certificate for jenkins website.
+```bash
+# I use vi, you might use nano or anything else you're comfortable with
+vi setup.sh
+```
+Now run setup:
+```bash
+./setup.sh
+```
+Now go to https://your_instance in the browser and paste in the last line output by the `setup.sh` script into the administration password in the browser. If the browser times out go to the same url again (make sure it's https). That's it!
+
+## Sources
+* https://wiki.jenkins-ci.org/display/JENKINS/Installing+Jenkins+on+Ubuntu
+* https://github.com/hughperkins/howto-jenkins-ssl
+* https://wiki.jenkins-ci.org/display/JENKINS/Running+Jenkins+behind+Apache
---
+++ setup.sh
@@ -0,0 +1,120 @@
+#!/bin/bash
+#################################################################
+## Set the values in this section
+#################################################################
+HOSTNAME='ec2-52-63-456-123.ap-southeast-2.compute.amazonaws.com'
+COUNTRY='AU'
+STATE='Australian Capital Territory'
+CITY='Canberra'
+ORG='MyCompany'
+ORG_UNIT='IT'
+EMAIL='somebody@gmail.com'
+#################################################################
+
+set -e 
+set -x
+
+sudo apt-get update
+sudo apt-get upgrade -y
+
+wget -q -O - https://pkg.jenkins.io/debian/jenkins-ci.org.key | sudo apt-key add -
+sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
+sudo apt-get update
+sudo apt-get install jenkins -y
+
+sudo apt-get install apache2 -y
+sudo a2enmod proxy
+sudo a2enmod proxy_http
+sudo a2enmod headers
+sudo a2enmod ssl
+
+# configure apache for ssl proxying
+sudo tee /etc/apache2/sites-enabled/ssl.conf <<EOF
+LoadModule ssl_module modules/mod_ssl.so
+LoadModule proxy_module modules/mod_proxy.so
+Listen 443
+<VirtualHost *:443>
+  <Proxy "*">
+    Order deny,allow
+    Allow from all
+  </Proxy>
+
+  SSLEngine             on
+  SSLCertificateFile    /etc/ssl/certs/my-cert.pem
+  SSLCertificateKeyFile /etc/ssl/private/my-key.pem
+
+  # this option is mandatory to force apache to forward the client cert data to tomcat
+  SSLOptions +ExportCertData
+
+  Header always set Strict-Transport-Security "max-age=63072000; includeSubdomains; preload"
+  Header always set X-Frame-Options DENY
+  Header always set X-Content-Type-Options nosniff
+
+  ProxyPass / http://localhost:8080/ retry=0 nocanon
+  ProxyPassReverse / http://localhost:8080/
+  ProxyPreserveHost on
+  AllowEncodedSlashes NoDecode
+  RequestHeader set X-Forwarded-Proto "https"
+  RequestHeader set X-Forwarded-Port "443"
+
+  LogFormat "%h (%{X-Forwarded-For}i) %l %u %t \"%r\" %>s %b \"%{Referer}i\" \"%{User-Agent}i\""
+  ErrorLog /var/log/apache2/ssl-error_log
+  TransferLog /var/log/apache2/ssl-access_log
+</VirtualHost>
+EOF
+
+## print out jenkins password for initial admin login
+sudo cat /var/lib/jenkins/secrets/initialAdminPassword
+
+## The script above uses the default ssl certificates for apache2
+## (and these will work but in the browser you will be warned about
+## the certificates being insecure). Now we make a self-signed certificate
+## that at least matches the desired host name:
+
+cd ~
+# we are now in /home/ubuntu
+
+#generate a password
+CA_PASSWORD=`< /dev/urandom tr -dc _A-Z-a-z-0-9 | head -c${1:-32}`
+
+# create server certificates (public and p`)
+tee server-config <<EOF
+# OpenSSL configuration file.
+[ req ]
+prompt = no
+distinguished_name          = req_distinguished_name
+
+[ req_distinguished_name ]
+C=TheCountry
+ST=TheState
+L=TheCity
+CN=TheHostname
+O=TheOrganization
+OU=TheOrgUnit
+emailAddress=TheEmail
+EOF
+sed -i -e "s/TheCountry/$COUNTRY/g" server-config
+sed -i -e "s/TheState/$STATE/g" server-config
+sed -i -e "s/TheCity/$CITY/g" server-config
+sed -i -e "s/TheHostname/$HOSTNAME/g" server-config
+sed -i -e "s/TheOrganization/$ORG/g" server-config
+sed -i -e "s/TheOrgUnit/$ORG_UNIT/g" server-config
+sed -i -e "s/TheEmail/$EMAIL/g" server-config
+
+openssl genrsa -out key.pem  2048 # creates key.pem
+openssl req -sha256 -new -key key.pem -out csr.pem -config server-config
+openssl x509 -req -days 9999 -in csr.pem -signkey key.pem -out cert.pem -passin "pass:$CA_PASSWORD"
+rm csr.pem
+rm server-config
+
+sudo cp cert.pem /etc/ssl/certs/my-cert.pem
+sudo cp key.pem /etc/ssl/private/my-key.pem
+
+sudo service apache2 restart
+set +x
+echo '**********************************************************'
+echo '** CA password (you probably don'"'"'t need it):'
+echo $CA_PASSWORD
+echo '**********************************************************'
+echo '** Initial administration password to enter into browser:'
+sudo cat /var/lib/jenkins/secrets/initialAdminPassword
